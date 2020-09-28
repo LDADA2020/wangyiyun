a7ac7f0df788484a2040ba7ec120b421175c1564
androidmumo <androidmumo@outlook.com>
2020-09-28 21:36:10 +0800

    发现页面完成

--- src/main.js
+++ src/main.js
@@ -2,9 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
-Vue.prototype.$http = axios;
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import Mint from 'mint-ui';
+import 'mint-ui/lib/style.css'
 
+Vue.prototype.$http = axios;
 Vue.config.productionTip = false
+Vue.use(ElementUI);
+Vue.use(Mint);
 
 new Vue({
   render: h => h(App),
