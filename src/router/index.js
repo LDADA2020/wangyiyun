b863547883d3c0b7ecbbf0d3f535bc7884f1aeb6
LDADA2020 <1763959776@qq.com>
2020-09-29 19:10:13 +0800

    周二晚上7点

--- src/router/index.js
+++ src/router/index.js
@@ -11,31 +11,50 @@ const router = new VueRouter({
         },
         {
             path: '/mine',
-            component: () => import('../views/mine/index.vue'),
+            component: () =>
+                import ('../views/mine/index.vue'),
         },
         {
             path: '/find',
-            component: () => import('../views/find/index.vue')
+            component: () =>
+                import ('../views/find/index.vue')
         },
         {
             path: '/village',
-            component: () => import('../views/village/index.vue')
+            component: () =>
+                import ('../views/village/index.vue'),
+            redirect: '/village/square',
+            children: [{
+                path: 'square',
+                name: 'Square',
+                component: () =>
+                    import ("../views/village/square/index.vue")
+            }, {
+                path: 'concer',
+                name: 'Concer',
+                component: () =>
+                    import ("../views/village/concer/index.vue")
+            }]
         },
         {
             path: '/video',
-            component: () => import('../views/video/index.vue')
+            component: () =>
+                import ('../views/video/index.vue')
         },
         {
             path: '/search',
-            component: () => import('../views/search/index.vue')
+            component: () =>
+                import ('../views/search/index.vue')
         },
         {
             path: '/audio',
-            component: () => import('../views/audio/index.vue')
+            component: () =>
+                import ('../views/audio/index.vue')
         },
         {
             path: '/login',
-            component: () => import('../views/login/index.vue')
+            component: () =>
+                import ('../views/login/index.vue')
         }
     ],
     linkActiveClass: 'active'
