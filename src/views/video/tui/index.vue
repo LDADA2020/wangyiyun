fac58aa520119c5a2688fc15b5b9cf6e15c94aef
xie981115 <2246967974@qq.com>
2020-09-30 13:45:42 +0800

    9.30

--- src/views/video/tui/index.vue
+++ src/views/video/tui/index.vue
@@ -1,45 +1,16 @@
 <!--  -->
 <template>
   <div>
-    <div class="content">
-    <div v-for="item in mv" :key="item">{{item}}</div>
-      <img  v-for="item in mv" :key="item" :src="item" alt="" @click="getpic" />
+    <video v-show="flag" autoplay="autoplay" src="" ref="videourl"></video>
+    <div class="content" v-for="item in mv" :key="item.id">
+      <img :src="item.cover" alt="" @click="getdetail(item.id)" />
       <div class="con">
-        <p>333654165454468455466514</p>
-        <img class="tu2" src="../../../assets/pic2.gif" alt="" />
+        <p>{{ item.name }}</p>
+        <img class="tu2" src="https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1601435146&di=8b93204e2f10cf9d48ff521311273b5c&src=http://img.redocn.com/sheji/20160327/fugubaozhibeijinggaodangCDguangpan_6058028.jpg" alt="" />
       </div>
       <div class="pin">
-        <img class="tu3" src="../../../assets/pic2.gif" alt="" />
-        <span>罗伊左</span>
-        <div class="biao">
-          <i class="el-icon-chat-line-round"></i><i class="el-icon-more"></i>
-        </div>
-      </div>
-    </div>
-    <div class="content">
-      <img src="../../../assets/pic1.gif" alt="" />
-      <div class="con">
-        <p>333654165454468455466514</p>
-        <img class="tu2" src="../../../assets/pic2.gif" alt="" />
-      </div>
-      <div class="pin">
-        <img class="tu3" src="../../../assets/pic2.gif" alt="" />
-        <span>罗伊左</span>
-        <div class="biao">
-          <i class="el-icon-chat-line-round"></i><i class="el-icon-more"></i>
-        </div>
-      </div>
-    </div>
-
-    <div class="content">
-      <img src="../../../assets/pic1.gif" alt="" />
-      <div class="con">
-        <p>333654165454468455466514</p>
-        <img class="tu2" src="../../../assets/pic2.gif" alt="" />
-      </div>
-      <div class="pin">
-        <img class="tu3" src="../../../assets/pic2.gif" alt="" />
-        <span>罗伊左</span>
+        <img class="tu3" :src="item.cover" alt="" />
+        <span>{{item.artistName}}</span>
         <div class="biao">
           <i class="el-icon-chat-line-round"></i><i class="el-icon-more"></i>
         </div>
@@ -53,7 +24,8 @@ export default {
   components: {},
   data() {
     return {
-        mv:[]
+      flag:false,
+      mv: [],
     };
   },
   computed: {},
@@ -64,23 +36,46 @@ export default {
         method: "get",
         url: "/api/top/mv",
       }).then((res) => {
-        console.log(res.data);
-        for (let i = 0; i < 6; i++) {
-        //   this.mv.push(res.data.data[i].cover);
-        }
+        console.log(res);
+        this.mv = res.data.data;
       });
     },
+    getdetail(id) {
+      // alert(id);
+      this.$http({
+        method: "get",
+        url: "/api/mv/url?id=" + id,
+      }).then((res) => {
+        console.log(res, 111);
+        this.flag=true;
+        this.$refs.videourl.src = res.data.data.url;
+        window.scrollTo(0,0);
+        
+      });
+      this.$http({
+        method: "get",
+        url: "/api/comment/mv?id="+id,
+      }).then((res) => {
+        console.log(res,333);
+      });
+    }
   },
   created() {
     this.getpic();
+    // this.getdetail();
   },
   mounted() {},
 };
 </script>
 <style scoped>
 .content {
-  margin-bottom: 15px;
+  margin-bottom: 20px;
   background: white;
+}
+video{width: 100%;
+}
+.content video {
+  width: 100%;
 }
 .content img {
   width: 100%;
@@ -95,12 +90,17 @@ export default {
 
 .con p {
   display: inline-block;
-  width: 80%;
+  width: 90%;
+  overflow: hidden;    
+text-overflow:ellipsis;    
+white-space: nowrap;
   float: left;
 }
 
 .con .tu2 {
   width: 30px;
+   height:30px;
+  border-radius: 50%;
   display: inline-block;
   float: right;
   margin-top: 5px;
@@ -114,6 +114,8 @@ export default {
 }
 .pin .tu3 {
   width: 30px;
+   height:30px;
+  border-radius: 50%;
   display: inline-block;
   margin-top: 5px;
 }
